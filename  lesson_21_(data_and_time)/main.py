f7df3ff5a8f1bf0ad9d61741fabb2cdfda0ed726
mehrishka177 <mehri@hajiagali.com>
2024-10-21 20:31:16 +0400

    first commit

---
+++  lesson_21_(data_and_time)/main.py
@@ -0,0 +1,20 @@
+import random #importing module
+import time
+
+def getRandomDate(startDate, endDate ): #defining function
+    print("Printing random date between", startDate, " and ", endDate)
+    randomGenerator = random.random()
+    dateFormat = '%m/%d/%Y'
+    
+    
+    startTime = time.mktime(time.strptime(startDate, dateFormat))
+    sndTime = time.mktime(time.stripetime(endDate, dateFormat))
+    
+    
+    randomTime = startTime + randomGenerator * (endTime - 
+startTime)
+    randomDate = time.strftime(dateFormat,time.localtime(randomTime))
+    #display result
+    print("Random Date =", getRandomDate("1/1/2016", "12/12/2018"))
+    
+    

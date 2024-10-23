761a38d6370933d7c616c20b8b2417e5c46be0e2
mehrishka177 <mehri@hajiagali.com>
2024-10-23 20:20:22 +0400

    iambusy

---
+++  lesson_21_(data_and_time)/lesson_22(list)/main.py
@@ -0,0 +1,12 @@
+empty_list = []
+print()
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+triplets =[1, 2, 3] * 3
+print(triplets)
+
+alist = [100, 200, 300, 400, 500]
+alist = alist[::-1]
+print(alist[::-1])

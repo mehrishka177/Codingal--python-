c89a54c9ee22d72c43951f9b693687dda51ca35f
mehrishka177 <mehri@hajiagali.com>
2024-10-28 20:21:53 +0400

    dhngkjfsnh

---
+++  lesson_21_(data_and_time)/lesson_22(list)/lesson_23_(tuple)/main.py
@@ -0,0 +1,13 @@
+#Create a tuple with diffrent data types 
+tuplex = ("tuple", False,3.2, 1)
+
+#create a tuple 
+tuplex = (4, 6, 2, 8, 3, 1)
+print(tuplex)
+
+ 
+tuplex = tuplex + (9,)
+print("tuplex")
+
+tuple1 = (50, 10, 60, 70, 50)
+print(tuple.count)

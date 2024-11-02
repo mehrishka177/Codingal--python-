79d1dc16da5616a629f3744467bdb9603b5eaaab
mehrishka177 <mehri@hajiagali.com>
2024-11-02 10:37:17 +0400

    submited

---
+++  lesson_21_(data_and_time)/lesson_22(list)/lesson_23_(tuple)/lesson_24_(dublicates)/hw.py/lesson_25/diying.py
@@ -0,0 +1,8 @@
+arry_num = arr.array('i', [1, 2, 3, 5, 3, 7, 9, 3])
+print("original array:"+str(array_num))
+
+print("Number of occurences of the number 3 in the said array:"+str(array_num.count(3)))
+
+array_num.revurse()
+print("Reverse the order of the items:")
+print(str(array_num))

e30c9efdbae43553e8aee9e372b074199a52d3b5
mehrishka177 <mehri@hajiagali.com>
2024-11-02 10:22:16 +0400

    mn,mn

---
+++  lesson_21_(data_and_time)/lesson_22(list)/lesson_23_(tuple)/lesson_24_(dublicates)/hw.py/lesson_25/main.py
@@ -0,0 +1,11 @@
+my_set = {1, 2, 3}
+print(my_set)
+
+my_set = {1.0, "hello", (1, 2, 3)}
+print(my_set)
+
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
+
+my_set = set([1, 2, 3, 4, 3,2])
+print(my_set,"\n")

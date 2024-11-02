e27a3d274d2a4a5d033c10837dc332b31e804012
mehrishka177 <mehri@hajiagali.com>
2024-11-02 10:25:23 +0400

    kjjk

---  lesson_21_(data_and_time)/lesson_22(list)/lesson_23_(tuple)/lesson_24_(dublicates)/hw.py/lesson_25/main.py
+++  lesson_21_(data_and_time)/lesson_22(list)/lesson_23_(tuple)/lesson_24_(dublicates)/hw.py/lesson_25/main.py
@@ -9,3 +9,10 @@ print(my_set)
 
 my_set = set([1, 2, 3, 4, 3,2])
 print(my_set,"\n")
+
+num_set = set([0, 1, 3, 4, 5])
+print(num_set)
+
+num_set.pop()
+print("after removing the first element from the said set:")
+print(num_set,"\n")

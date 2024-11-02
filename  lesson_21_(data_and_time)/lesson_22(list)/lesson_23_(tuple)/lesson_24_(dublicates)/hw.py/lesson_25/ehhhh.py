b11bfeba9ed4314667f588d061d3710921e5c94c
mehrishka177 <mehri@hajiagali.com>
2024-11-02 10:30:33 +0400

    j

---
+++  lesson_21_(data_and_time)/lesson_22(list)/lesson_23_(tuple)/lesson_24_(dublicates)/hw.py/lesson_25/ehhhh.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"blue", "yello"}
+print("original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
